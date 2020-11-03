197dc8fa47cd8aa7a59ee25007e34f88c057463a
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2020-11-03 22:35:59 +0300

    Add files via upload

---
+++ py_example4.py
@@ -0,0 +1,20 @@
+#Rümeysa Coşkun
+#Python comment basit örneği.
+
+
+#this is a comment
+print("Hello World")
+
+print("Hello World")#This is a comment
+
+print("Hello World")#print("Hello World")
+
+#Hello
+#Everyone
+#How are you
+#Today
+
+"""
+This is a comment
+Do you understand.
+"""
